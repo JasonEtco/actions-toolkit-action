15871f1a281690c2eab2394cbe37eabbbe35d027
Jason Etcovitch <jasonetco@github.com>
2019-02-26 17:04:51 -0500

    Use the first argument as the file name

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { Toolkit } = require('actions-toolkit')
+const path = require('path')
 const tools = new Toolkit()
 
 // TODO: Detect if script exists and fails gracefully if it doesn't
-const scriptPath = `/.github/actions/${tools.context.event}.js`
-require(tools.workspace + scriptPath)(tools)
+const scriptPath = tools.arguments._[0] || `/.github/actions/${tools.context.event}.js`
+// Remove the file from the list of arguments
+if (tools.arguments._[0]) tools.arguments._.shift()
+require(path.join(tools.workspace, scriptPath))(tools)
